7adb65cca2d17e5a92915d23e6bc7b50a53e6746
ripamonti-daniele <ripamonti.daniele.08@itisdalmine.edu.it>
2025-02-16 17:32:31 +0100

    creazione file di lavoro

---
+++ codice_fiscale.py
@@ -0,0 +1,5 @@
+#file utilizzato per utilizzare le funzioni e realizzare il programma principale
+
+import funzioni
+from os import system
+system("cls")
---
+++ funzioni.py
@@ -0,0 +1,1 @@
+# file per creare le funzioni
