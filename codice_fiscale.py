9015838398bd6f09855f8890f043ad2002ef76db
ripamonti-daniele <ripamonti.daniele.08@itisdalmine.edu.it>
2025-02-21 19:40:54 +0100

    realizzate funzioni colonna b

--- codice_fiscale.py
+++ codice_fiscale.py
@@ -1,5 +1,19 @@
 #file utilizzato per utilizzare le funzioni e realizzare il programma principale
 
-import funzioni
+from funzioni import *
 from os import system
 system("cls")
+
+cognome = ChiediCognome()
+nome = chiediNome()
+data = chiediDataNascita()
+sesso = chiediSesso()
+c_cognome = calcolaCodiceCognome(cognome)
+c_nome = calcolaCodiceNome(nome)
+c_anno = calcolaCodiceAnno(data)
+c_mese = calcolaCodiceMese(data)
+c_giorno = calcolaCodiceGiorno(data, sesso)
+c_comune = calcolaCodiceComune()
+codice = (c_cognome + c_nome + c_anno + c_mese + c_giorno + c_comune).upper()
+c_controllo = calcolaCodiceControllo(codice)
+print(codice + c_controllo)
--- funzioni.py
+++ funzioni.py
@@ -3,7 +3,7 @@ import datetime
 #COLONNA C
 
 comuni = {
-    "Alpette","Lesa" ,"Galliate" ,"Bra","Moiola" ,"Valgrana","Castellero" ,"Robella" ,"Cartosio" , "Magnano" 
+    "Alpette","Lesa" ,"Galliate" ,"Bra","Moiola" ,"Valgrana","Castellero" ,"Robella" ,"Cartosio" , "Bergamo" 
 }
 def rimuoviLettereAccentate(s):
     sostituzioni = {
@@ -32,14 +32,12 @@ def chiedicomune(comuni):
         else:
             print("Errore, il comune non e valido")
 
-def calcolaCodiceMese():
+def calcolaCodiceMese(data):
     alfabeto = ("A", "B" ,"C", "D", "E", "H", "L", "M", "P", "R", "S", "T")
-    data_input = input("Inserisci la tua data di nascita (formato YYYY-MM-DD): ")
-    data_nascita = datetime.datetime.strptime(data_input, "%Y-%m-%d")
-    mese_nascita = data_nascita.month
+    mese_nascita = data.month
     for i in range(12):
-        if i == mese_nascita:
-            codice = alfabeto[i-1]
+        if i + 1 == mese_nascita:
+            codice = alfabeto[i]
             return codice
 
 
@@ -81,7 +79,9 @@ def ChiediCognome():
     while error:
         error = False
         cognome = input("Inserisci il tuo cognome: ").capitalize()
-        if not cognome.isdigit():
+        cognome = rimuoviLettereAccentate(cognome)
+        cognome = rimuoviSpazi(cognome)
+        if not cognome.isalpha():
             print("Errore: caratteri non validi")
             error = True
         else:
@@ -89,11 +89,9 @@ def ChiediCognome():
                 print("Errore: cognome troppo corto o troppo lungo")
                 error = True
             else:
-                rimuoviLettereAccentate()
-                rimuoviSpazi()
                 return cognome
             
-def  chiediDataNascita():
+def chiediDataNascita():
     error = True
     while error:
         error = False
@@ -120,7 +118,7 @@ def calcolaCodiceComune():
         "Castellero": "A755",
         "Robella": "A818",
         "Cartosio": "A881",
-        "Magnano": "A059"  
+        "Bergamo": "A794"  
     }
     error = True
     while error:
@@ -158,3 +156,76 @@ def calcolaCodiceCognome(cognome):
         risultato += "X"
     
     return risultato
+
+#colonna b
+def chiediNome():
+    while True:
+        nome = input("Inserisci il tuo nome: ").capitalize().strip()
+        nome = rimuoviLettereAccentate(nome)
+        nome = rimuoviSpazi(nome)
+        if not nome.isalpha():
+            print("Errore: caratteri non validi")
+        elif len(nome) < 2 or len(nome) > 30:
+            print("Errore lunghezza nome (min 2 max 30)")
+        else:
+            return nome
+        
+def chiediSesso():
+    while True:
+        sesso = input("Inserisci il tuo sesso (m/f): ").strip().lower()
+        
+        if sesso != "m" and sesso != "f":
+            print("errore: inserisci m oppure f")
+        else:
+            return sesso
+        
+def calcolaCodiceGiorno(data, sesso):   
+    g = data.day
+    if sesso == "f":
+        g += 40
+    g = str(g)
+    if len(g) == 1:
+        g = "0" + g
+    
+    return g
+
+def calcolaCodiceControllo(codice):
+    pari = ""
+    dispari = ""
+    for i in range(len(codice)):
+        if (i + 1) % 2 == 0:
+            pari += codice[i]
+        else:
+            dispari += codice[i]
+            
+    conv_dispari = {
+        "0": 1, "1": 0, "2": 5, "3": 7, "4": 9, "5": 13, "6": 15, "7": 17, "8": 19, "9": 21, 
+        "A": 1, "B": 0, "C": 5, "D": 7, "E": 9, "F": 13, "G": 15, "H": 17, "I": 19, "J": 21, "K": 2, "L": 4, "M": 18,
+        "N": 20, "O": 11, "P": 3, "Q": 6, "R": 8, "S": 12, "T": 14, "U": 16, "V": 10, "W": 22, "X": 25, "Y": 24, "Z": 23
+    }
+    
+    conv_pari = {}
+    n = 0
+    for i in ("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"):
+        conv_pari[i] = n
+        if i == "9":
+            n = 0
+        else:
+            n += 1
+    
+    tot = 0
+    for i in pari:
+        tot += conv_pari[i]
+    for i in dispari:
+        tot += conv_dispari[i]
+        
+    while tot >= 26:
+        tot -= 26
+    
+    conversioni = {}
+     
+    lettere = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    for i in range(26):
+        conversioni[i] = lettere[i]
+        
+    return conversioni[tot]
